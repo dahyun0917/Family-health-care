92ef37baf454ddcf411707961ee2d77d73170e7f
dahyun0917 <dahyun0917@naver.com>
2023-05-03 19:12:31 +0900

    Initial Commit

---
+++ Family-health-care/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Family-health-care
+//
+//  Created by 이다현 on 2023/05/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
