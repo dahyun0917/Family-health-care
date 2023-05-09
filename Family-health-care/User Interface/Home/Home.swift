c7b703765c8d38e682b421ab314cbbede3bbc4e0
dahyun0917 <dahyun0917@naver.com>
2023-05-09 20:20:33 +0900

    !HOTFIX : 누락된 파일 추가

---
+++ Family-health-care/User Interface/Home/Home.swift
@@ -0,0 +1,21 @@
+//
+//  Home.swift
+//  Family-health-care
+//
+//  Created by 이다현 on 2023/05/07.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+//        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        SnsView()
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        Home()
+    }
+}
