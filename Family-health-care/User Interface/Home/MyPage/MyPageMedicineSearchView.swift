7725c2ca78220190684684018e1ef16527790f0a
yang1318 <amy1353@naver.com>
2023-05-16 22:09:23 +0900

    Design : 기초 UI 완성 - MyPageMedicineSearchView

---
+++ Family-health-care/User Interface/Home/MyPage/MyPageMedicineSearchView.swift
@@ -0,0 +1,40 @@
+//
+//  MyPageMedicineSearchView.swift
+//  Family-health-care
+//
+//  Created by 양선아 on 2023/05/16.
+//
+
+import SwiftUI
+
+struct MyPageMedicineSearchView: View {
+    
+    var body: some View {
+        ZStack{
+            Color.mainBeige.edgesIgnoringSafeArea(.all)
+            VStack{
+                HStack{
+                    Image(systemName: "magnifyingglass")
+                        .padding(20)
+                        .foregroundColor(Color.lightGray)
+                    Text("글루패스트")
+                    Spacer()
+                }
+                .frame(maxWidth:348,maxHeight:50)
+                .background().cornerRadius(12)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 12)
+                        .stroke(Color.lightGray, lineWidth: 2))
+                
+                SearchResultRow(medicine: medicineSamples[0])
+                Spacer()
+            }
+        }
+    }
+}
+
+struct MyPageMedicineSearchView_Previews: PreviewProvider {
+    static var previews: some View {
+        MyPageMedicineSearchView()
+    }
+}
