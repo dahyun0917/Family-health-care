9410c39b9f9a95a65b0eaf466e8b3d96391f5fae
seona.Yang <75965560+yang1318@users.noreply.github.com>
2023-05-23 17:42:02 +0900

    Design : 기초 UI 완성 - MyPageEditMedicineState

---
+++ Family-health-care/User Interface/Home/MyPage/MyPageEditMedicineStateView.swift
@@ -0,0 +1,179 @@
+//
+//  MyPageEditMedicineStateView.swift
+//  Family-health-care
+//
+//  Created by 양선아 on 2023/05/23.
+//
+
+import SwiftUI
+
+struct MyPageEditMedicineStateView: View {
+    let medicine: Medicine
+    @State private var currentDate = Date()
+    var body: some View {
+        VStack {
+            HStack {
+                Spacer()
+                Text("취소").foregroundColor(Color.mediumGray).padding(.trailing, 10)
+                Text("삭제").foregroundColor(.red).padding(.trailing, 40)
+            }
+            Spacer().frame(height: 20)
+            Divider()
+            VStack {
+                Spacer().frame(height: 30)
+                VStack {
+                    HStack {
+                        Text("💊 드시는 약")
+                            .font(.headline)
+                            .padding([.leading, .bottom])
+                        Spacer()
+                    }
+                    medicineSettingView(medicine: medicine)
+                }
+                Spacer()
+                    .frame(height: 40)
+            }
+            Divider()
+            VStack {
+                Spacer()
+                    .frame(height: 40)
+                VStack {
+                    HStack {
+                        Text("🕐 몇시에 드시는 약인가요?")
+                            .font(.headline)
+                            .padding([.leading, .bottom])
+                        Spacer()
+                    }
+                    DatePicker("", selection: $currentDate, displayedComponents: [.hourAndMinute])
+                        .labelsHidden()
+                }
+                Spacer()
+                    .frame(height: 40)
+            }
+            Divider()
+            VStack {
+                Spacer()
+                    .frame(height: 40)
+                VStack {
+                    HStack {
+                        Text("🗓 요일을 선택해 주세요")
+                            .font(.headline)
+                            .padding([.leading, .bottom])
+                        Spacer()
+                    }
+                    HStack {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(Color.mainBlue, lineWidth: 2)
+                                .frame(width: 40, height: 40)
+                            Text("월")
+                                .foregroundColor(Color.mainBlue)
+                        }
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(Color.mainBlue, lineWidth: 2)
+                                .frame(width: 40, height: 40)
+                            Text("화")
+                                .foregroundColor(Color.mainBlue)
+                        }
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(Color.mainBlue, lineWidth: 2)
+                                .frame(width: 40, height: 40)
+                            Text("수")
+                                .foregroundColor(Color.mainBlue)
+                        }
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(Color.mainBlue, lineWidth: 2)
+                                .frame(width: 40, height: 40)
+                            Text("목")
+                                .foregroundColor(Color.mainBlue)
+                        }
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(Color.mainBlue, lineWidth: 2)
+                                .frame(width: 40, height: 40)
+                            Text("금")
+                                .foregroundColor(Color.mainBlue)
+                        }
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(Color.mainBlue, lineWidth: 2)
+                                .frame(width: 40, height: 40)
+                            Text("토")
+                                .foregroundColor(Color.mainBlue)
+                        }
+                        
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 10)
+                                .foregroundColor(Color.mainBlue)
+                                .frame(width: 40, height: 40)
+                            Text("일")
+                                .foregroundColor(Color.mainWhite)
+                        }
+                    }
+                }
+            }
+            Spacer()
+            ZStack {
+                RoundedRectangle(cornerRadius: 10)
+                    .foregroundColor(Color.mainBlue)
+                    .frame(height: 60)
+                Text("설정 완료")
+                    .foregroundColor(Color.mainWhite)
+            }
+        }
+    }
+}
+
+struct medicineSettingView : View {
+    let medicine: Medicine
+    var body: some View {
+        HStack {
+            Image(medicine.medicineImage)
+                .resizable()
+                .scaledToFill()
+                .frame(width: 100, height: 100)
+                .clipped()
+                .overlay(
+                    RoundedRectangle(cornerRadius: 0)
+                        .stroke(Color.lightGray, lineWidth: 2))
+                .padding(.leading, 15)
+            
+            VStack(alignment: .leading) {
+                HStack {
+                    Text(medicine.medicineName)
+                        .font(.headline)
+                        .fontWeight(.medium)
+                        .padding(.bottom, 6)
+                    Spacer()
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 10)
+                            .foregroundColor(Color.mainBlue)
+                            .frame(width: 33, height: 20)
+                        Text("변경")
+                            .font(.caption)
+                            .foregroundColor(Color.mainWhite)
+                    }
+                    .padding(.bottom, 6)
+                    .padding(.trailing, 20)
+                }
+                Text(medicine.description)
+                    .font(.footnote)
+                    .foregroundColor(Color.mediumGray)
+            }
+        }
+        .frame(maxWidth:348,maxHeight:130)
+        .background().cornerRadius(12)
+        .overlay(
+            RoundedRectangle(cornerRadius: 12)
+                .stroke(Color.lightGray, lineWidth: 2))
+    }
+}
+
+struct MyPageEditMedicineStateView_Previews: PreviewProvider {
+    static var previews: some View {
+        MyPageEditMedicineStateView()
+    }
+}
