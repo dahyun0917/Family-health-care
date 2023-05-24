1db441f11831ca986505c3b08482a11fb1453e1f
Seokhyeon <dltjr0703@gmail.com>
2023-05-24 17:23:35 +0900

    Feat : Firebase 채팅 내역 가져오는 기능 추가

--- Family-health-care/Model/Chats.swift
+++ Family-health-care/Model/Chats.swift
@@ -15,16 +15,19 @@ final class Chats: ObservableObject{
         let combined = botMessages as [any Message] + userMessages as [any Message]
         return combined.sorted(by: { $0.id < $1.id })
     }
-    init(){
-        // 임시 데이터
-        var temp_bot = [
-            BotMessage(id:Date(timeIntervalSince1970: 0),body:"안녕 첫 대화야"),
-            BotMessage(id:Date(timeIntervalSince1970: 1),body:"아침 약으로 진통제와 혈압약 먹을 시간이야!", answers:["먹었어", "까먹었어", "싫어"])]
+    init(token:String){
+//                // 임시 데이터
+//                var temp_bot = [
+//                    BotMessage(id:Date(timeIntervalSince1970: 0),body:"안녕 첫 대화야"),
+//                    BotMessage(id:Date(timeIntervalSince1970: 1),body:"아침 약으로 진통제와 혈압약 먹을 시간이야!", answers:["먹었어", "까먹었어", "싫어"])]
+//
+//                self.botMessages = temp_bot
+//                        self.userMessages = temp_user
         
-        self.botMessages = temp_bot
-//        self.userMessages = temp_user
         
         //firebase에서 사용자의 채팅 내역을 가져옴
+        fetchBotMessage(token)
+        fetchUserMessage(token)
     }
     
 }
@@ -38,7 +41,62 @@ extension Chats{
         }
     }
     
-    func fetchMessage(message:any Message){
-        //add message
+    func fetchBotMessage(_ token : String){
+        let db = Firestore.firestore()
+        let user = db.collection("users").document(token)
+        let botMessages = user.collection("BotMessage")
+        
+        //        // user information
+        //        user.getDocument(){ (doc, error) in
+        //            guard error == nil, let doc = doc, doc.exists else{
+        //                print("Error: \(error?.localizedDescription ?? "")")
+        //                return
+        //            }
+        //            let info = doc.data()
+        //            if let info = info{
+        //                print(info)
+        //            }
+        //        }
+        
+        botMessages.getDocuments(){ (botMessageSnapshot, error) in
+            guard error == nil, let botMessageSnapshot = botMessageSnapshot else{
+                print("Error: \(error?.localizedDescription ?? "")")
+                return
+            }
+            
+            for botMessageDoc in botMessageSnapshot.documents{
+                guard let timestamp = botMessageDoc["time"] as? Timestamp else {
+                    print("Error: Unable to cast 'time' to Timestamp.")
+                    continue
+                }
+                let body = botMessageDoc.data()["body"] as? String ?? ""
+                let answers = botMessageDoc.data()["answers"] as? [String] ?? []
+                self.botMessages.append(BotMessage(id:timestamp.dateValue(), body:body, answers: answers))
+            }
+        }
+        
+    }
+    func fetchUserMessage(_ token : String){
+        let db = Firestore.firestore()
+        let user = db.collection("users").document(token)
+        let userMessages = user.collection("UserMessage")
+        
+        userMessages.getDocuments(){ (userMessageSnapshot, error) in
+            guard error == nil, let userMessageSnapshot = userMessageSnapshot else{
+                print("Error: \(error?.localizedDescription ?? "")")
+                return
+            }
+            
+            for userMessageDoc in userMessageSnapshot.documents{
+                guard let timestamp_id = userMessageDoc["time"] as? Timestamp, let timestamp_associated = userMessageDoc["associated"] as? Timestamp
+                else {
+                    print("Error: Unable to cast 'time', 'associated' to Timestamp.")
+                    continue
+                }
+                let body = userMessageDoc.data()["body"] as? String ?? ""
+                self.userMessages.append(UserMessage(id:timestamp_id.dateValue(), body:body, associated: timestamp_associated.dateValue()))
+            }
+        }
+        
     }
 }
