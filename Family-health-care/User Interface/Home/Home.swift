d858b703d6ecbe79ebab1c4c68f1466d4d84221c
Seokhyeon <dltjr0703@gmail.com>
2023-05-24 21:34:57 +0900

    Chore : Home, Build 수정

--- Family-health-care/User Interface/Home/Home.swift
+++ Family-health-care/User Interface/Home/Home.swift
@@ -8,9 +8,33 @@
 import SwiftUI
 
 struct Home: View {
+    @EnvironmentObject private var chat : Chats
     var body: some View {
+//        VStack{
+//            let botMessages = chat.allMessages
+//            ForEach(botMessages, id:\.id) { message in
+//                Text("\(message.id)")
+//                /*@START_MENU_TOKEN@*/Text(message.body)/*@END_MENU_TOKEN@*/
+//            }
+//        }
 //        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-        SnsView()
+//        SnsView()
+        CharacterView()
+//        VStack{
+//            ForEach(chat.allMessages, id:\.0){ messages in
+//                Text(messages.1)
+//            }
+//        }
+        
+        // firebase test code
+//        NavigationView{
+//            NavigationLink(destination: testtest().environmentObject(test)){
+//                VStack{
+//                    Text("\(test.born)")
+//                    Text("\(test.t)")
+//                }
+//            }
+//        }
     }
 }
 
