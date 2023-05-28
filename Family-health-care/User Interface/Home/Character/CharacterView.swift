45ac4501606f4ee2533f5044a09c7fb7e9d413c5
Seokhyeon <dltjr0703@gmail.com>
2023-05-29 02:43:23 +0900

    Design : 캐릭터 변경 View 변경
    ScrollView -> TabView

--- Family-health-care/User Interface/Home/Character/CharacterView.swift
+++ Family-health-care/User Interface/Home/Character/CharacterView.swift
@@ -9,7 +9,8 @@ import SwiftUI
 
 struct CharacterView: View {
     @State private var gifName : String = "man_gif"
-
+    @State private var selected = 0
+    
     var body: some View {
         ZStack{
             Color.clear
@@ -18,46 +19,68 @@ struct CharacterView: View {
                 let parentWidth = parent_proxy.size.width
                 let outerWidth = parent_proxy.size.width-30
                 let innerWidth = parent_proxy.size.width-50
+                let innerHeight = parent_proxy.size.height-50
                 let parentHeight = parent_proxy.size.height
                 
                 
                 VStack(alignment: .center){
-                    GeometryReader { geometry_Gif in
-                        
-                        NavigationView{
-                            ScrollViewReader{scrollProxy in
-                                ScrollView(.horizontal, showsIndicators: false){
-                                    
-                                    HStack(spacing:0){
-                                        ZStack{
-                                            GifView(gifName: "man_gif")
-                                                .frame(width:geometry_Gif.size.width, height:geometry_Gif.size.height)
-                                                .aspectRatio(contentMode: .fit)
-                                            
-                                            
-                                            NavigationLink(destination: SickView()){
-                                                Symbol("magnifyingglass", scale:.large, color: .black)
-                                            }.position(x:geometry_Gif.size.width*0.85, y:geometry_Gif.size.height*0.85)
-                                        }
-                                        .frame(width:geometry_Gif.size.width, height:geometry_Gif.size.height).id("start")
-                                        CharacterSelectionView(gifName:$gifName).frame(width:geometry_Gif.size.width, height:geometry_Gif.size.height)
-                                    }
-                                    
-                                }.onChange(of: gifName) { _ in
-                                    withAnimation{
-                                        scrollProxy.scrollTo("start", anchor: .leading)
+                    NavigationView{
+                        VStack{
+                            HStack(spacing: 30) {
+                                Button("내 캐릭터") { withAnimation { selected = 0 } }
+                                    .frame(width: 100)
+                                    .background(
+                                        RoundedRectangle(cornerRadius: 10)
+                                            .foregroundColor(Color.mainLightBeige)
+                                            .overlay(
+                                                RoundedRectangle(cornerRadius: 10)
+                                                    .fill(selected == 0 ? Color.mainLightBeige : Color.mainBeige)
+                                            )
+                                    )
+                                    .foregroundColor(Color.black)
+                                Button("캐릭터 변경") {
+                                    withAnimation {
+                                        selected = 1
                                     }
                                 }
-                                .background(Color.mainBeige)
+                                .frame(width: innerWidth*0.25)
+                                .background(
+                                    RoundedRectangle(cornerRadius: 10)
+                                        .fill(selected == 1 ? Color.mainLightBeige : Color.mainBeige)
+                                )
+                                .foregroundColor(Color.black)
+                                
                             }
-                            
+                            .padding()
+                            .background(Color.mainBeige)
+                            GeometryReader { geometry_Gif in
+                                TabView(selection: $selected) {
+                                    ZStack{
+                                        GifView(gifName: $gifName)
+                                            .frame(width:geometry_Gif.size.width, height:geometry_Gif.size.height)
+                                            .aspectRatio(contentMode: .fit)
+                                        NavigationLink(destination: SickView()){
+                                            Symbol("magnifyingglass", scale:.large, color: .black)
+                                                .padding(5)
+                                                .background(Circle().fill(Color.mainLightBeige))
+                                        }.position(x:geometry_Gif.size.width*0.85, y:geometry_Gif.size.height*0.85)
+                                    }
+                                    .frame(width:geometry_Gif.size.width, height:geometry_Gif.size.height).tag(0)
+                                    CharacterSelectionView(gifName:$gifName)
+                                        .frame(width:geometry_Gif.size.width, height:geometry_Gif.size.height).tag(1)
+                                }
+                            }
                         }
-                    }
-                    .onAppear{UIScrollView.appearance().isPagingEnabled = true}
+                        .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+                        .background(Color.mainBeige)
+                        .onChange(of: gifName){ _ in
+                            withAnimation{ selected = 0 }
+                        }
+                    }.navigationBarHidden(true)
                     Spacer()
                     
                     Chat()
-                        .frame(width:innerWidth, height:innerWidth*0.8)
+                        .frame(width:innerWidth, height:min(innerHeight*0.8, innerWidth*0.8))
                     
                     medicineTakenView()
                         .frame(width:innerWidth, height:50)
