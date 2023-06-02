ddacf75629946fbde3289896c2cabd9c296f639c
akuby21 <39649594+akuby21@users.noreply.github.com>
2023-06-02 17:05:46 +0900

    Feat : User, Family Model Firebase connect
    User, Family클래스 생성.
    userId가 key인 상태로 Firestore에 저장된 데이터로 User 생성.
    로그인 기준 User의 소속 Family reference로 가족 데이터 불러오기 기능 구현

--- Family-health-care/Model/Family.swift
+++ Family-health-care/Model/Family.swift
@@ -5,9 +5,53 @@
 //  Created by 류지원 on 2023/05/24.
 //
 
-//import Foundation
-//
-//final class Family{
-//    var users : [User]
-//    
-//}
+import Foundation
+import FirebaseFirestore
+
+final class Family: ObservableObject{
+    @Published var users : [User] = []
+    var flag : Bool = true
+    init(user : String){
+        getUserData(userId: user)
+        print(users)
+    }
+    
+    func getUserData (userId : String){
+        let db = Firestore.firestore()
+        var names : [String] = []
+        db.collection("users").document(userId).getDocument(){(doc, error) in
+            guard error == nil, let doc = doc, doc.exists else{
+                print("Error: \(error?.localizedDescription ?? "")")
+                return
+            }
+            let info = doc.data()
+            
+            if let info = info{
+                if let family = info["family"] as? DocumentReference{
+                    family.getDocument{ (doc, error) in
+                        guard error == nil, let doc = doc, doc.exists else{
+                            print("Error: \(error?.localizedDescription ?? "")")
+                            return
+                        }
+                        //print("\(type(of: doc.data()!))")
+                        if let users = doc["users"]! as? [String]{
+                            for i in users{
+                                names.append(i)
+                            }
+                            for name in names{
+                                let _ = User(token:name){ use in
+                                    self.users.append(use)
+                                }
+                            }
+                            
+                        }
+
+                    }
+                    
+                }
+            }
+           
+        }
+        
+    }
+}
