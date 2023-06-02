4a4717b3131b085b8bfbe13b7807757462fe296b
akuby21 <39649594+akuby21@users.noreply.github.com>
2023-06-02 17:06:41 +0900

    feat : NavigationView added
    하단의 네비게이션 바 구현

---
+++ Family-health-care/User Interface/Home/navView.swift
@@ -0,0 +1,42 @@
+//
+//  TabView.swift
+//  Family-health-care
+//
+//  Created by 류지원 on 2023/05/30.
+//
+
+import SwiftUI
+
+struct navView: View {
+    var body: some View {
+        TabView{
+            CharacterView().environmentObject(Chats(token:"admin"))
+                .tabItem{
+                    Image(systemName:"figure.socialdance")
+                    Text("캐릭터")
+                }
+            MyPageView()
+                .tabItem{
+                    Image(systemName: "person.circle")
+                    Text("마이페이지")
+                }
+            SnsView(isWrite: false)
+                .tabItem{
+                    Image(systemName:"shareplay")
+                    Text("SNS")
+                }
+            FamilyPageView()
+                .tabItem{
+                    Image(systemName: "figure.2.and.child.holdinghands")
+                    Text("가족")
+                }
+            
+        }
+    }
+}
+
+struct navView_Previews: PreviewProvider {
+    static var previews: some View {
+        navView()
+    }
+}
