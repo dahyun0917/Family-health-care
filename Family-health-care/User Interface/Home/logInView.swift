d0e221b7b9bad582336067a98052a848199fb3b8
Seokhyeon <dltjr0703@gmail.com>
2023-06-12 22:03:30 +0900

    Design : 로그인 디자인 변경

--- Family-health-care/User Interface/Home/logInView.swift
+++ Family-health-care/User Interface/Home/logInView.swift
@@ -9,35 +9,43 @@ import SwiftUI
 struct logInView: View {
     @EnvironmentObject private var userLoader : UserLoader
     @State private var flag : Bool = false
+    @State private var loading_gif : String = "loading"
     
     var body: some View {
         
-        
-        NavigationStack {
+        if flag{
+            if let user = userLoader.user{
+                navView()
+                    .environmentObject(Family(user:user))
+                    .environmentObject(userLoader)
+                    .environmentObject(Chats(token: user.userId))
+                    .environmentObject(MedicationsForPainOfParts())
+                    .environmentObject(Medicines())
+                    .environmentObject(Posts())
+                    .environmentObject(Storys())
+            }
+        }
+        else{
             ZStack {
                 Color.mainBeige.ignoresSafeArea(.all)
-                if let user = userLoader.user{
-                    
-                    NavigationLink(destination: CharacterView().environmentObject(Family(user:user)).environmentObject(userLoader)
-                    ,isActive: $flag,label:{})
-                        
-                    
+                VStack{
+//                    GifView(gifName:)
+                    GifView(gifName:$loading_gif)
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width:200)
                 }
+                Image("sprout")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width:450)
+                    .rotationEffect(.degrees(40))
+                    .offset(x:-180, y:200)
             }
-        }.onAppear{
-            DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 3) {
-                flag = true
+            .onAppear{
+                DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 2.7) {
+                    flag = true
+                }
             }
         }
     }
-    
 }
-
-struct logInView_Previews: PreviewProvider {
-    static var previews: some View {
-        //logInView()
-        Text("hi")
-    }
-}
-
-
