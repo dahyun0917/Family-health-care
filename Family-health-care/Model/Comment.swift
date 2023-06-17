608c96ddfe4a47c0fe643474dbfeadbeaabdf351
dahyun0917 <dahyun0917@naver.com>
2023-06-17 14:50:53 +0900

    Fix : SNS 관련 데이터 모델에 createdByImg 추가

--- Family-health-care/Model/Comment.swift
+++ Family-health-care/Model/Comment.swift
@@ -11,19 +11,6 @@ struct Comment: Codable,Identifiable {
     let id: UUID = UUID()
     var content: String = ""
     let createdBy: String
+    var createdByImg : String
     var createdAt: Date
 }
-let CommentSamples = [
-    Comment(content:"오 정말 대단하다.",createdBy: "asdf",createdAt: Date()),
-    Comment(content:"오 정말 대단하지 않다.",createdBy: "qwer",createdAt: Date()),
-    Comment(content:"오 정말 ",createdBy: "zxcv",createdAt: Date()),
-    Comment(content:"오 정",createdBy: "dfgh",createdAt: Date()),
-    Comment(content:"오 ",createdBy: "qwe",createdAt: Date()),
-    Comment(content:"말 대단하다.",createdBy: "asdewrf",createdAt: Date()),
-    Comment(content:"대단하다.",createdBy: "assfdf",createdAt: Date()),
-    Comment(content:"단하다.",createdBy: "er",createdAt: Date()),
-    Comment(content:"하다.",createdBy: "tuj",createdAt: Date()),
-    Comment(content:"다.",createdBy: "cxz",createdAt: Date()),
-    Comment(content:"오 정다.",createdBy: "qwhhg",createdAt: Date())
-    
-]
--- Family-health-care/Model/Post.swift
+++ Family-health-care/Model/Post.swift
@@ -14,31 +14,8 @@ struct Post: Codable,Identifiable{
     var img: String
     var comment : [Comment] = []
     let createdBy: String
+    var createdByImg : String
     var createdAt: Date
 }
 
-let PostSamples = [
-    Post(title: "안년",content: "안녀아아녀아아녕아녀아녀아녀아녀아녀아녀아녕",img:"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",comment:CommentSamples,createdBy: "dahyun",createdAt:Date()),
-    Post(title: "히히히힣",content: "하하하하하하하하ㅏ하하하하하ㅏ하하하하하ㅏ하하하",img:"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",comment:CommentSamples,createdBy: "dahyun",createdAt:Date()),
-    Post(title: "테스트테스트테스트",content: "하하하하하하하하ㅏ하하하하하ㅏ하하하하하ㅏ하하하",img:"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",comment:CommentSamples,createdBy: "dahyun",createdAt:Date()),
-    Post(title: "나는 테스트테스트",content: "하하하하하하하하ㅏ하하하하하ㅏ하하하하하ㅏ하하하",img:"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",comment:CommentSamples,createdBy: "dahyun",createdAt:Date()),
-    Post(title: "테스트할거야아",content: "하하하하하하하하ㅏ하하하하하ㅏ하하하하하ㅏ하하하",img:"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",comment:CommentSamples,createdBy: "dahyun",createdAt:Date()),
-    Post(title: "우하하하",content: "하하하하하하하하ㅏ하하하하하ㅏ하하하하하ㅏ하하하",img:"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",comment:CommentSamples,createdBy: "dahyun",createdAt:Date())
-    
-]
 
-//final class Posts: ObservableObject{
-//    @Published var posts : [Post] = []
-//    init(){
-//        var temp_post = [
-//            Post(title: "안년",content: "안녀아아녀아아녕아녀아녀아녀아녀아녀아녀아녕",img:"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",comment:CommentSamples,createdBy: "dahyun",createdAt:Date()),
-//            Post(title: "히히히힣",content: "하하하하하하하하ㅏ하하하하하ㅏ하하하하하ㅏ하하하",img:"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",comment:CommentSamples,createdBy: "dahyun",createdAt:Date()),
-//            Post(title: "테스트테스트테스트",content: "하하하하하하하하ㅏ하하하하하ㅏ하하하하하ㅏ하하하",img:"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",comment:CommentSamples,createdBy: "dahyun",createdAt:Date()),
-//            Post(title: "나는 테스트테스트",content: "하하하하하하하하ㅏ하하하하하ㅏ하하하하하ㅏ하하하",img:"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",comment:CommentSamples,createdBy: "dahyun",createdAt:Date()),
-//            Post(title: "테스트할거야아",content: "하하하하하하하하ㅏ하하하하하ㅏ하하하하하ㅏ하하하",img:"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",comment:CommentSamples,createdBy: "dahyun",createdAt:Date()),
-//            Post(title: "우하하하",content: "하하하하하하하하ㅏ하하하하하ㅏ하하하하하ㅏ하하하",img:"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",comment:CommentSamples,createdBy: "dahyun",createdAt:Date())]
-//        
-//        self.posts = temp_post
-//    }
-//    
-//}
